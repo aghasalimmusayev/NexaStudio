6ac2ddc6aeada356646972a7a7647143fd485f1a
Aghasalim Musayev <aga77ams@gmail.com>
2026-06-12 13:08:09 +0400

    Replace anchor tag with Next.js Link component

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link"
+
 export default function Logo() {
     return (
-        <a href="/" className="text-xl font-bold text-white">NexaStudio</a>
+        <Link href="/" className="text-xl font-bold text-white">NexaStudio</Link>
     )
 }
